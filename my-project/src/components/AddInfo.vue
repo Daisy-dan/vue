000b2ecc995dd7d6d2ca00653bdf22dbcd89c7a6
Daisy-dan <wudan@yimei180.com>
2018-03-29 17:07:41 +0800

    update

--- my-project/src/components/AddInfo.vue
+++ my-project/src/components/AddInfo.vue
@@ -37,6 +37,53 @@
             </el-form-item>
           </el-col>
         </el-row>
+
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="即时配送">
+              <el-switch v-model="delivery"></el-switch>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :span="24">
+            <el-form-item label="活动性质">
+              <el-checkbox-group v-model="type">
+                <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+                <el-checkbox label="地推活动" name="type"></el-checkbox>
+                <el-checkbox label="线下主题活动" name="type"></el-checkbox>
+                <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+              </el-checkbox-group>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="特殊资源">
+              <el-radio-group v-model="resource">
+                <el-radio label="线上品牌商赞助"></el-radio>
+                <el-radio label="线下场地免费"></el-radio>
+              </el-radio-group>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="活动形式">
+              <el-input type="textarea" v-model="desc"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :span="8" :offset="3">
+            <el-button type="primary" @click="onSubmit">立即创建</el-button>
+            <el-button>取消</el-button>
+          </el-col>
+        </el-row>
       </el-form>
     </div>
   </el-container>
@@ -62,7 +109,16 @@ export default {
         age: '',
         date: '',
         role: '',
+        delivery: false,
+        type: '',
+        resource: '',
+        desc: '',
       }
+  },
+  methods: {
+    onSubmit() {
+        console.log('submit!')
+    }
   }
 }
 
